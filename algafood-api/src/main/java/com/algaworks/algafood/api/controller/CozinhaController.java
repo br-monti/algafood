33b6c5570029958b930c8ef5c7502f9b5dd6c886
Bruno <bruno.smsouza@gmail.com>
2021-03-26 20:30:30 -0300

    4.20

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,7 +3,11 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +15,7 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas")
 public class CozinhaController {
 
 	@Autowired
@@ -22,4 +26,24 @@ public class CozinhaController {
 		return cozinhaRepository.listar();
 	}
 	
+//	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+//	public CozinhasXmlWrapper listarXml() {
+//		return new CozinhasXmlWrapper(cozinhaRepository.listar());
+//	}
+
+	@GetMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+//		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+//		return ResponseEntity.ok(cozinha);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
+		
+		return ResponseEntity
+				.status(HttpStatus.FOUND)
+				.headers(headers)
+				.build();
+	}
 }
