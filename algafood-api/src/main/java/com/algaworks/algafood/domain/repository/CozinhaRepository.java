a94e9b9c4dc1c5060c84fdd191ca10b730c2a0b1
Bruno <bruno.smsouza@gmail.com>
2021-04-04 17:00:49 -0300

    5.4

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,21 +1,11 @@
-  
 package com.algaworks.algafood.domain.repository;
 
-import java.util.List;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 
 @Component
-public interface CozinhaRepository {
+public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-	List<Cozinha> listar();
-	List<Cozinha> consultarPorNome(String nome);
-	Cozinha buscar(Long id);
-	
-	
-	Cozinha salvar(Cozinha cozinha);
-	void remover(Long id);
-	
 }
