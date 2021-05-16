35aee71d6c86e155f4366ece973d1d247146eea2
Bruno <bruno.smsouza@gmail.com>
2021-05-16 20:26:15 -0300

    8.14

--- algafood-api/src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,19 +1,11 @@
 package com.algaworks.algafood.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
-//@ResponseStatus(value = HttpStatus.NOT_FOUND) //, reason = "Entidade não encontrada")
-public abstract class EntidadeNaoEncontradaException extends ResponseStatusException {
+public abstract class EntidadeNaoEncontradaException extends NegocioException {
 
 	private static final long serialVersionUID = 1L;
 
-	public EntidadeNaoEncontradaException(HttpStatus status, String mensagem) {
-		super(status, mensagem);
-	}
-
 	public EntidadeNaoEncontradaException(String mensagem) {
-		this(HttpStatus.NOT_FOUND, mensagem);
+		super(mensagem);
 	}
 	
 }
